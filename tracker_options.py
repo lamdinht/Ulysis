bed7934c4e61f62c9cd96f4223d4542e89bb552d
pda <63649337+duyanh131212@users.noreply.github.com>
2022-03-23 22:55:13 -0400

    Add files via upload

--- tracker_options.py
+++ tracker_options.py
@@ -1,11 +1,34 @@
-option = {"INPUT_FPS" : "30" , "OUTPUT_FORMAT" : "mp4", "YOLOV4-TINY" : "False", "FRAMEWORK" : "tf" , "MODEL" : "Yolov3" , 
+from tkinter import N
+
+
+
+setting = {"INPUT_FPS" : "30" , "OUTPUT_FORMAT" : "mp4", "YOLOV4-TINY" : "False", "FRAMEWORK" : "tf" , "MODEL" : "Yolov3" , 
             "IOU Threshold" : "0.45" , "Score" : "0.55" , "Dont_show" : "False" , "Info" : "True"} 
 
-default = input("Default setting? (Y/N) : ")
-if default == "Y":
-    print("Using default options")
-elif default == "N":
-    for key,value in option.items():
-        option[key] = input(key + " : ")
+settingOption = {"1" : "INPUT_FPS" , "2" : "OUTPUT_FORMAT", "3" : "YOLOV4-TINY", "4" : "FRAMEWORK" , "5" : "MODEL" , 
+            "6" : "IOU Threshold"  , "7" : "Score"  ,  "8" : "Dont_show"  , "9" : "Info"  , 0 : "All" } 
 
-print(option)
+
+
+print("\nDefault Setting : \n")
+print(setting)
+print("\n")
+
+default = input("Use default setting? (Y/N) : ")
+
+if default == "Y":
+    print("\nUsing default setting\n")
+elif default == "N":
+    print("\nChoose setting to change : \n")
+    for key in settingOption:
+        print(str(key) + " " + settingOption[key])
+    keychosen = input("\nType separate by space : ").split()
+    if "0" in keychosen: 
+        print("Changing all setting option")
+        for key,value in setting.items():
+            setting[key] = input(key + " : ")
+    else: 
+        for i in keychosen:
+            setting[settingOption[i]] = input(settingOption[i] + " : ")
+
+print(setting)
